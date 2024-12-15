e026f3ca5f7320cde1979e7e73b22da6a02ec50c
Kevin Bett <prodigy4614@gmail.com>
2024-12-15 12:29:07 +0300

    updated author model with update/delet method

--- models/author.py
+++ models/author.py
@@ -3,10 +3,18 @@ class Author:
         self.id = id
         self.name = name
 
-    def create_author(cursor, name):
-        """Create an author and return the ID."""
+    @staticmethod
+    def create(cursor, name):
         cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
         return cursor.lastrowid
+
+    @staticmethod
+    def update(cursor, author_id, name):
+        cursor.execute('UPDATE authors SET name = ? WHERE id = ?', (name, author_id))
+
+    @staticmethod
+    def delete(cursor, author_id):
+        cursor.execute('DELETE FROM authors WHERE id = ?', (author_id,))
 
     def __repr__(self):
         return f'<Author {self.name}>'
