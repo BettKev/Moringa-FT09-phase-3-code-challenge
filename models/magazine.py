e327b2ebb2c3faa9cbb1b157a5a4c07b1edf08a3
Kevin Bett <prodigy4614@gmail.com>
2024-12-15 12:29:31 +0300

    updated magazine model with update/delet method

--- models/magazine.py
+++ models/magazine.py
@@ -1,33 +1,21 @@
 class Magazine:
     def __init__(self, id, name, category):
         self.id = id
-        self._name = name
-        self._category = category
+        self.name = name
+        self.category = category
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or not (2 <= len(value) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        self._name = value
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, value):
-        if not isinstance(value, str) or len(value) <= 0:
-            raise ValueError("Category must be a non-empty string.")
-        self._category = value
-
-    def create_magazine(cursor, name, category):
-        """Create a magazine and return the ID."""
+    @staticmethod
+    def create(cursor, name, category):
         cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (name, category))
         return cursor.lastrowid
+
+    @staticmethod
+    def update(cursor, magazine_id, name, category):
+        cursor.execute('UPDATE magazines SET name = ?, category = ? WHERE id = ?', (name, category, magazine_id))
+
+    @staticmethod
+    def delete(cursor, magazine_id):
+        cursor.execute('DELETE FROM magazines WHERE id = ?', (magazine_id,))
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
